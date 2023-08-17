f9dd616e0ba179d32e8cebc9e9686903a1cafb81
TechPro424 <smsvra6@gmail.com>
2023-08-17 19:38:47 +0530

    first commit

---
+++ src/main/java/org/refabricators/gaia/GrimoireOfGaiaDataGenerator.java
@@ -0,0 +1,11 @@
+package org.refabricators.gaia;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class GrimoireOfGaiaDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
